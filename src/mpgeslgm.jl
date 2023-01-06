f73d3503b48536750acb9a12aa040c33cb740eff
Tim Kelley <ctk@ncsu.edu>
2023-01-06 17:17:20 -0500

    More cleanup

--- src/mpgeslgm.jl
+++
@@ -1,8 +0,0 @@
-function mpgeslgm(MPHF::MPHTest, b)
-eta=1.e-8
-x0=zeros(n)
-V=zeros(n,20)
-kout=kl_gmres(x0, b, MPhatv, V, eta, MPhptv; pdata=MPHF, side="left");
-println(kout.reshist)
-return kout.sol
-end
