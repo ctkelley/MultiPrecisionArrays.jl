a3d88478fbc51f39376d170f920d67dbbfa70a60
Documenter.jl <documenter@juliadocs.github.io>
2023-01-09 22:30:16 +0000

    build based on cda11bb

---
+++ v0.0.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MPArrays.jl","page":"Home","title":"MPArrays.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"C. T. Kelley","category":"page"},{"location":"","page":"Home","title":"Home","text":"MPArrays.jl is a package for iterative refinement. ","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is mostly for research at this point. It will get more interesting as hardware support for Float16 comes online.","category":"page"}]
+}
