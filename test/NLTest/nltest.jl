68e5f85397ea58d07c019cb9a0297357495f9180
Tim Kelley <ctk@ncsu.edu>
2023-06-08 13:41:01 -0400

    Fix CI woes

--- test/NLTest/nltest.jl
+++ test/NLTest/nltest.jl
@@ -5,7 +5,8 @@ Use MPArrays to solve H-equation
 """
 function nltest()
 hpass=nlhtest();
-hpass16=nlhtest
+hpass16=nlhtest16()
+return hpass && hpass16
 end
 
 #
@@ -33,6 +34,36 @@ lmp=length(mpnout.history)
 lmph=length(mphnout.history)
 hpass=(llu==lmp) && (lmph == lmp)
 end
+
+#
+# H-equation test: Float16 Jacobian vs IR-GMRES for singular Jacobian
+#
+function nlhtest16(n=32,c=1.0)
+FV=zeros(n);
+JV=zeros(Float32,n,n);
+JV16=zeros(Float16,n,n);
+JVMP=MPArray(JV);
+JVMPH=MPHArray(JV);
+x0=ones(n);
+tol=1.e-8
+hdata = heqinit(x0, c);
+nout=nsol(heqf!, x0, FV, JV, heqJ!;
+          rtol=tol, atol=tol, pdata = hdata, sham = 1, jfact=lu!)
+nout16=nsol(heqf!, x0, FV, JV16, heqJ!;
+          rtol=tol, atol=tol, pdata = hdata, sham = 1, jfact=lu!)
+mpnout=nsol(heqf!, x0, FV, JVMP, jheqmp!;
+          rtol=tol, atol=tol, pdata = hdata, sham = 1, jfact=mplu!)
+mphnout=nsol(heqf!, x0, FV, JVMPH, jheqmp!;
+          rtol=tol, atol=tol, pdata = hdata, sham = 1, jfact=mpglu!)
+llu=length(nout.history)
+ll16=length(nout16.history)
+lmp=length(mpnout.history)
+lmph=length(mphnout.history)
+halfpass = (ll16 == 21) && (llu == lmph) && (lmp > llu)
+return halfpass
+end
+
+
 
 function jheqmp!(JVMP, FV, x,hdata)
 JV=JVMP.AH
