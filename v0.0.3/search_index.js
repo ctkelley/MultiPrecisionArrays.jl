281b89f1569d5520196cc663ea38145f7995ea88
Documenter.jl <documenter@juliadocs.github.io>
2023-08-07 19:31:03 +0000

    build based on e6df320

---
+++ v0.0.3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultiPrecisionArrays.jl-v0.0.3","page":"Home","title":"MultiPrecisionArrays.jl v0.0.3","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"C. T. Kelley","category":"page"},{"location":"","page":"Home","title":"Home","text":"MultiPrecisionArrays.jl is a package for iterative refinement. ","category":"page"},{"location":"","page":"Home","title":"Home","text":"This is mostly for research at this point. It will get more interesting as hardware support for Float16 comes online.","category":"page"}]
+}
