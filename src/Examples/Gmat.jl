ceef3b9758096cfe11a766b8e27f98609fce4a28
Tim Kelley <ctk@ncsu.edu>
2023-12-05 11:15:56 -0500

    Example update

--- src/Examples/Gmat.jl
+++ src/Examples/Gmat.jl
@@ -1,19 +1,20 @@
 """
 Gmat: discrete Green's operator for -d^2/dx^2 in 1D
 """
-function Gmat(n)
-    h = 1.0 / (n + 1.0)
-    X = collect(h:h:1.0-h)
-    G = [greens(x, y) for x in X, y in X]
+function Gmat(n,T=Float64)
+    onet=T(1.0)
+    h = onet / (n + onet)
+    X = collect(T, h:h:1.0-h)
+    G = [greens(x, y, onet) for x in X, y in X]
     G .*= h
     return G
 end
 
-function greens(x, y)
+function greens(x, y, onet)
     if x > y
-        gf = y * (1.0 - x)
+        gf = y * (onet - x)
     else
-        gf = x * (1.0 - y)
+        gf = x * (onet - y)
     end
     return gf
 end
