d768d09fb66afa5500ee13ebed1dc5226eb554e4
Tim Kelley <ctk@ncsu.edu>
2023-12-05 11:47:55 -0500

    Bug fix

--- src/Examples/Gmat.jl
+++ src/Examples/Gmat.jl
@@ -3,8 +3,10 @@ Gmat: discrete Green's operator for -d^2/dx^2 in 1D
 """
 function Gmat(n,T=Float64)
     onet=T(1.0)
-    h = onet / (n + onet)
-    X = collect(T, h:h:1.0-h)
+# fudge fixes a corner case if T=Float32
+    fudge=eps(T)*T(5.0)
+    h = onet/ (n + onet)
+    X = collect(T, h:h:1.0-h+fudge)
     G = [greens(x, y, onet) for x in X, y in X]
     G .*= h
     return G
