df2b70fb2d9c6380359069a630bc3c07fa0c7c8a
Tim Kelley <ctk@ncsu.edu>
2024-03-17 13:54:09 -0400

    skip ci

--- docs/make.jl
+++ docs/make.jl
@@ -38,7 +38,7 @@ makedocs(
         "Half Precision and Krylov-IR" => Any["Half_1.md",], 
         "More than you want to know" => 
            Any["Details/Termination.md", "Details/Interprecision_1.md",
-                Details/Extended.md",],
+                "Details/Extended.md",],
         "MPArray Constructors" => 
            Any["functions/MPArray.md","functions/MPGArray.md",
                "functions/MPBArray.md",],
