502600b5782a5ca202b559b8838baa10dc30b2db
Tim Kelley <ctk@ncsu.edu>
2024-09-30 15:14:56 -0400

    fix internal docs

--- src/Factorizations/hlu!.jl
+++ src/Factorizations/hlu!.jl
@@ -12,7 +12,7 @@ I "fixed" the code to be Float16 only and fixed pivoting
 to only MaxRow.
 
 All I did in the factorization
-was thread the critical loop with FLoops.@floop and
+was thread the critical loop with OhMyThreads:tforeach and
 put @simd in the inner loop. For larger problems (n > 128)
 these changes got me a 2-10x speedup
 on my Mac M2 Pro with 8 performance cores. I'm happy.
