c8765439a679183a1141d450841431ed04c3a7f2
Tim Kelley <ctk@ncsu.edu>
2024-10-16 16:35:59 -0400

    CI tweak

--- test/DetailsTest/AbsArray.jl
+++ test/DetailsTest/AbsArray.jl
@@ -3,7 +3,7 @@ A=rand(10,10); x=ones(10); b=A'*x;
 B=A';
 MPB=mplu(B)
 z=MPB\b
-absok = (norm(x - z,Inf) < 1.e-11)
+absok = (norm(x - z,Inf) < 1.e-10)
 absok || println(norm(x-z,Inf))
 return absok
 end
