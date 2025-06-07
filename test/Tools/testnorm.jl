d2f1c3a3aeb73a372c429973d5aebb1378f2bb8a
Tim Kelley <ctk@ncsu.edu>
2025-06-07 14:06:02 -0400

    Massive CI for new GMRES-IR function + kill dead code

--- test/Tools/testnorm.jl
+++ test/Tools/testnorm.jl
@@ -1,4 +1,9 @@
 function testnorm(delnorm, TA, alpha)
+#
+# This function is used for a couple problems where the convergence
+# of GMRES-IR is poor if TW=TR=Float32 and TF=Float64. That is why
+# the tolerances are large in that case. 
+#
     if TA == Float64
         normok = (alpha < 750.0) ? (delnorm < 1.e-13) : (delnorm < 1.e-10)
     else
